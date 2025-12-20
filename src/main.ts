a575cc0a68e37d74eb5e02707dde0fad73b06ecf
David <davidgimenez1997clase@gmail.com>
2025-12-20 08:10:40 +0100

    feat(ui): add bootstrap and fontawesome

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { APP_INITIALIZER } from "@angular/core";
+import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { APP_ICONS } from "./app/core/icons/fontawesome-icons";
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+    ...appConfig,
+    providers: [
+        ...(appConfig.providers ?? []),
+        {
+            provide: APP_INITIALIZER,
+            multi: true,
+            deps: [FaIconLibrary],
+            useFactory: (library: FaIconLibrary) => () => library.addIcons(...APP_ICONS),
+        },
+    ],
+}).catch((err) => console.error(err));
